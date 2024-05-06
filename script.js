12b1d83d91162ecf67dacd57580f25951e2e80c2
sMC-Lean <125336469+sMC-Lean@users.noreply.github.com>
2024-05-06 21:43:05 +1000

    completed changes to game logic and tested

--- script.js
+++ script.js
@@ -1,62 +1,108 @@
 "use strict";
+const startButton = document.querySelector(".start-button");
+const userControlSection = document.querySelector(".control-div");
+const humanImage = document.querySelector(".human-image");
+const computerImage = document.querySelector(".computer-image");
+const announceText = document.querySelector(".announcement");
+const humanScoreBoard = document.querySelector(".human-score");
+const computerScoreBoard = document.querySelector(".computer-score");
+const choices = ["rock", "paper", "scissors"];
 
-// define initial score values
 let [humanScore, computerScore] = [0, 0];
 
-// array of possible choices for use within the random choice function;
-const choices = ["rock", "paper", "scissors"];
+// helper functions to make the reset function smaller and more readable;
+function hideImages() {
+  humanImage.style.opacity = 0;
+  computerImage.style.opacity = 0;
+  humanImage.src = "";
+  computerImage.src = "";
+}
+
+function hideAnnouncementText() {
+  announceText.textContent = "";
+  announceText.classList.add("hidden");
+}
+
+function resetScores() {
+  humanScore = 0;
+  computerScore = 0;
+  humanScoreBoard.textContent = "";
+  computerScoreBoard.textContent = "";
+}
+
+// function to start a new game or clear all the inputs and reset to a new game once a winning condition has been set;
+function startGame(event) {
+  hideImages();
+  hideAnnouncementText();
+  resetScores();
+  userControlSection.classList.remove("hidden");
+}
+
+// a single round, triggered by the click event on the user's chosen button;
+
+function playRound(humanChoice) {
+  const computerChoice = getComputerChoice();
+  checkRoundWinner(humanChoice, computerChoice);
+  announceText.classList.toggle("hidden");
+  if (humanScore === 5 || computerScore === 5) declareWinner();
+  else setTimeout(nextRound, 2000);
+}
+
+// resets the user input section to accept another choice while the game is valid;
+function nextRound() {
+  hideAnnouncementText();
+  userControlSection.classList.toggle("hidden");
+}
 
 // generate random integer between 0 and 2 (inclusive) and return a random choice from the array;
 const getComputerChoice = function () {
   const choiceInd = Math.floor(Math.random() * 3);
-  return choices[choiceInd];
+  const computerChoice = choices[choiceInd];
+  computerImage.src = `./images/${computerChoice}.webp`;
+  computerImage.style.opacity = 1;
+  return computerChoice;
 };
 
-// function to take a choice from a prompt, validate it, and return it;
-const getHumanChoice = function () {
-  let valid = false;
-  let choice = prompt(
-    "make a choice from: paper, scissors or rock"
-  ).toLowerCase();
-  while (!valid) {
-    if (choice !== "paper" && choice !== "scissors" && choice !== "rock") {
-      choice = prompt("choose again: paper, scissors or rock").toLowerCase();
-    } else {
-      valid = true;
-      return choice;
-    }
-  }
-  valid = false;
-};
+// helper functions to call for the possible win/loss/tie conditions, breaks down the larger evaluation;
 
-// helper functions to call for the possible win/loss/tie conditions;
-// in the game loop;
 const humanWins = function () {
   humanScore++;
-  console.log(`You win this round!`);
+  humanScoreBoard.textContent = humanScore;
+  announceText.textContent = "You Win this round!";
 };
 const computerWins = function () {
   computerScore++;
-  console.log(`You lose this round!`);
+  computerScoreBoard.textContent = computerScore;
+  announceText.textContent = "Computer won this round!";
 };
 
 const tie = function () {
-  console.log(`TIE!`);
-};
-// declaring the final winner;
-const declareWinner = function (winner, score) {
-  console.log(
-    `${winner} ${
-      winner === "You" ? "are" : "is"
-    } the winner! the final score was ${score}`
-  );
+  announceText.textContent = "TIE!";
 };
 
-// function for a single round of the game;
-const playRound = function (humanChoice, computerChoice) {
-  console.log(`you chose ${humanChoice},`);
-  console.log(`computer chose ${computerChoice},`);
+// declaring the final winner if a score has reached 5;
+const declareWinner = function () {
+  humanImage.style.opacity = 0;
+  computerImage.style.opacity = 0;
+  humanImage.src = "";
+  computerImage.src = "";
+  if (humanScore === 5) {
+    announceText.textContent = "You Win!";
+    humanImage.src = "./images/winner.webp";
+    humanImage.style.opacity = 1;
+  } else if (computerScore === 5) {
+    announceText.textContent = "Computer Won!";
+    computerImage.src = "./images/winner.webp";
+    computerImage.style.opacity = 1;
+  }
+  setTimeout(function () {
+    hideAnnouncementText();
+    startButton.classList.toggle("hidden");
+  }, 3000);
+};
 
+// helper function : abstracts the evaluation away from the round function;
+const checkRoundWinner = function (humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     tie();
   } else if (humanChoice === "rock") {
@@ -80,28 +126,19 @@ const playRound = function (humanChoice, computerChoice) {
   }
 };
 
-// playRound(getHumanChoice(), getComputerChoice());
+// event listener starts the clean game or calls the reset if a game has already played;
+startButton.addEventListener("click", function () {
+  startButton.classList.toggle("hidden");
+  announceText.textContent = "First to 5 Wins...";
+  announceText.classList.toggle("hidden");
+  setTimeout(startGame, 2000);
+});
 
-// function with a loop to play 5 rounds of the game;
-const playGame = function () {
-  let winner = "";
-  let score = "";
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-  if (computerScore > humanScore) {
-    winner += "Computer";
-    score += `${computerScore} to ${humanScore}`;
-  } else if (computerScore < humanScore) {
-    winner += "You";
-    score += `${humanScore} to ${computerScore}`;
-  } else if (computerScore === humanScore) {
-    winner += "NO-ONE";
-    score += `${humanScore} to ${computerScore}`;
-  }
-  declareWinner(winner, score);
-};
-
-// calling the final function to play 5 rounds;
-
-// playGame();
+// event listener for taking the user input nd using the id to set the user's played choice image;
+userControlSection.addEventListener("click", function (event) {
+  const humanChoice = event.target.id;
+  humanImage.src = `./images/${humanChoice}.webp`;
+  humanImage.style.opacity = 1;
+  userControlSection.classList.toggle("hidden");
+  playRound(humanChoice);
+});
